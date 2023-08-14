61fba7bc042f528891c46f60fae53e787beda7f6
Zohanubis <phdh141003@gmail.com>
2023-08-14 10:34:01 +0700

    Test

--- Ex3/Controllers/DNController.cs
+++ Ex3/Controllers/DNController.cs
@@ -3,31 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-using Ex3.Models.User;
+using Ex3.Models;
 
 namespace Ex3.Controllers
 {
     public class DNController : Controller
     {
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+        [HttpGet]
+        public ActionResult DangKy()
+        {
+            return View();
+        }
         // GET: DN
         [HttpPost]
         public ActionResult DangNhap(string name, string password)
         {
             if("yennh".Equals(name) && "123456".Equals(password))
             {
-                Session["user"] = new User() { login = name, Us}
+                Session["user"] = new User() { login = name, UserName = "Phạm Hồ Đăng Huy" };
             }
+            return RedirectToAction("Index", "Home");
         }
 
         [HttpPost]
-        public ActionResult DangKy(User user)
+        public ActionResult DangKy(string name, string password, string rt_password)
         {
-            if (user.UserName.Length >= 5 && user.PassWord.Length >= 6 && user.PassWord == user.ReTypePassWord)
+            if (name.Length >= 5 && password.Length >= 6 && rt_password.Equals(password))
             {
                 // Handle registration logic here
-                return RedirectToAction("DangNhap");
+                return RedirectToAction("DangNhap","DN");
             }
-            return View("DangKy");
+            else { return View(); }
+            
+        }
+        public ActionResult DangXuat()
+        {
+            Session.Clear();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
--- Ex3/Models/User.cs
+++ Ex3/Models/User.cs
@@ -12,5 +12,9 @@ namespace Ex3.Models
         public string FullName { get; set; }
         public string Email { get; set; }
         public string Phone { get; set; }
+        public User()
+        {
+
+        }
     }
 }
